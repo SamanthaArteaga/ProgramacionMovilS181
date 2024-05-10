64cd69376e5d88d27a15856daa2202e7334584ac
SamanthaArteaga <121037868@upq.edu.mx>
2024-05-10 17:03:58 -0600

    Reglas para nombrar variables
    Se describe la importancia de la lógica en las variables.

--- 01-tipos/01-variables.js
+++ 01-tipos/01-variables.js
@@ -1,3 +1,10 @@
+//UpperCamelCase, camelCase, snake_case
 let nombre = "Hola Mundo";
+let NombreCompleto;
+let nombreCompleto;
+let nombre_completo;
+console.log(NombreCompleto);
 
-console.log(nombre);
+let apellido;
+let  animal;
+apellido = 'Schurmann';
